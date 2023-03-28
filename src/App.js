c6ab1c09889c29010df1241c2b21da9d85a75a7e
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-28 14:51:11 +0300

    new edits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Header from './components/Header/Header.jsx'
+import Hero from "./components/Hero/Hero.jsx"
 
 function App() {
   return <div className='wrapper'>
     <Header/>
+    <Hero/>
   </div>
 }
 
