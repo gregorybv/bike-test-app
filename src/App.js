d3ce790ab1a5505258710d175d05d16f9891a521
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-28 15:47:08 +0300

    add aos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react"
-import Header from './components/Header/Header.jsx'
+import Header from "./components/Header/Header.jsx"
 import Hero from "./components/Hero/Hero.jsx"
 
+import AOS from "aos"
+import "aos/dist/aos.css" // You can also use <link> for styles
+// ..
+AOS.init()
+
 function App() {
-  return <div className='wrapper'>
-    <Header/>
-    <Hero/>
-  </div>
+  return (
+    <div className='wrapper'>
+      <Header />
+      <Hero />
+    </div>
+  )
 }
 
 export default App
