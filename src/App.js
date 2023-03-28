bbe573028d7ad184eb1a1454fefff5ab1409ba5e
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-28 14:31:59 +0300

    new edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React from "react"
+import Header from './components/Header/Header.jsx'
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className='wrapper'>
+    <Header/>
+  </div>
 }
 
-export default App;
+export default App
